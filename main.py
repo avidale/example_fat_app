e668a161723ffa04f593c6fb52a2885de90e2a95
David Dale <ddale@yandex-team.ru>
2020-01-12 17:18:59 +0300

    git lfs and bert code

--- main.py
+++ main.py
@@ -1,25 +1,61 @@
+import torch
 import os
-import gensim
+import numpy as np
+
+from transformers import BertModel, BertConfig, BertTokenizer
 
 from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
 
-path_to_model = 'C:/Users/ddale/Downloads/NLP/rusvectores/model.model'
-print('loading ft')
-ft = gensim.models.fasttext.FastTextKeyedVectors.load(path_to_model)
-print('adjusting ft')
-ft.adjust_vectors()
+# model_dir = 'C:/Users/ddale/Downloads/NLP/rubert_deeppavlov/rubert_cased_L-12_H-768_A-12_v2'
+model_dir = 'models/rubert_cased_L-12_H-768_A-12_v2'
+
+bert_tokenizer = BertTokenizer.from_pretrained(os.path.join(model_dir, 'vocab.txt'))
+inv_voc = {idx: tok for tok, idx in bert_tokenizer.vocab.items()}
+
+bert_config = BertConfig.from_pretrained(os.path.join(model_dir, 'bert_config.json'))
+bert_model = BertModel.from_pretrained(os.path.join(model_dir, 'bert_model.bin'), config=bert_config)
+
+
+def normalize(v):
+    return v / sum(v**2)**0.5
+
+
+def bert_emb_cls(word):
+    with torch.no_grad():
+        raw, pooled = bert_model(torch.tensor([bert_tokenizer.encode(word)]))
+    return normalize(pooled[0].numpy())
+
+
+def bert_emb_pool(word):
+    with torch.no_grad():
+        raw, pooled = bert_model(torch.tensor([bert_tokenizer.encode(word)]))
+    return normalize(raw[0].numpy().mean(axis=0))
+
+
+def bert_cos(w1, w2, cls=False):
+    if cls:
+        bert_emb = bert_emb_cls
+    else:
+        bert_emb = bert_emb_pool
+    e1 = bert_emb(w1)
+    e2 = bert_emb(w2)
+    return np.dot(e1, e2) / np.sqrt(np.dot(e1, e1) * np.dot(e2, e2))
 
 
 @app.route('/', methods=['POST', 'GET'])
-def main_api():
-    if not request.json:
-        text = 'привет'
-    else:
+def main_api(text='привет'):
+    if request.json:
         text = request.json['text']
-    return jsonify({'text': text, 'result': ft[text].tolist()})
+    return jsonify({'text': text, 'result': bert_emb_pool(text).tolist()})
+
+
+@app.route('/<word>', methods=['GET'])
+def main_api_word(word='привет'):
+    return main_api(text=word)
 
 
 if __name__ == '__main__':
